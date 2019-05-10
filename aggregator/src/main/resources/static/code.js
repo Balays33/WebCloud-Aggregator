88d94e80824bbbd90c45e256c7713a46d68fcb74
Balays33 <40806995+Balays33@users.noreply.github.com>
2019-05-11 00:01:19 +0100

    finals

--- aggregator/src/main/resources/static/code.js
+++ aggregator/src/main/resources/static/code.js
@@ -1,56 +1,24 @@
-/*
-$$(document).on('deviceready', function() {
-    console.log("Device is ready!");
-    // automatt get geolocation to the progarm
-    
-    getTime();
-});
-*/
-
-/*
-$(document).ready(function(){
-    getTime();
-});
-*/
-/*
-function getTime( ){
-    var d = new Date();
-    var d1 = new Date().toLocaleTimeString(); // 11:18:48 AM
-    
-    document.getElementById("gettimes").innerHTML = d1;
-    console.log(d1);
-
-    var today = new Date().toLocaleDateString();
-       //  07-06-2016 06:38:34
-    //var dd = String(today.getDate()).padStart(2, '0');
-    //var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    //var yyyy = today.getFullYear();
-
-    //today = mm + '/' + dd + '/' + yyyy;
-    document.getElementById("getmount").innerHTML = today;
-    //document.write(today);
-
-    setTimeout(getTime, 1000);
-    
-}
-*/
-
 var myVar = setInterval(myTimer, 1000);
 
 function myTimer() {
   var d = new Date();
   document.getElementById("gettimes").innerHTML = d.toLocaleTimeString();
+  //document.getElementById("gettimes").innerHTML = d.getTime();
+  var autoUpdateTime = d.getTime();
+  console.log(autoUpdateTime);
+  sendDataIsoServerAUpdateTime(autoUpdateTime);
+  sendDataUtcServerAUpdateTime(autoUpdateTime);
+
+
 }
-function kiir(){
-    console.log('should wrk ');
-    document.getElementById("test").innerHTML = "Balazs";
-}
+
 
 
 function sendDataIsoServer(){
-    //console.log(Latitude, Longitude);
     var http = new XMLHttpRequest();
-    const url = 'http://isodate.kubernetes.cafe/1234567/isodate';
+    //const url = 'http://isodate.kubernetes.cafe/1234567/isodate';
+    const url = 'http://localhost:8081/timestamp/'+document.getElementById("Timestamp").value;
+    console.log('timestamp value : '+document.getElementById("Timestamp").value);
     http.open("GET", url);
     http.send();
     http.onreadystatechange = (e) => {
@@ -67,9 +35,9 @@ function sendDataIsoServer(){
 }
 
 function sendDataUtcServer(){
-    console.log('hu ha');
     var http = new XMLHttpRequest();
-    const url = 'http://localhost:8081/utcDate/1234567';
+    const url = 'http://localhost:8081/utcDate/'+document.getElementById("Timestamp").value;
+    console.log('timestamp value : '+document.getElementById("Timestamp").value);
     http.open("GET", url);
     http.send();
     http.onreadystatechange = (e) => {
@@ -85,3 +53,42 @@ function sendDataUtcServer(){
 
 }
 
+
+function sendDataIsoServerAUpdateTime(autoUpdateTime){
+    var http = new XMLHttpRequest();
+    //const url = 'http://isodate.kubernetes.cafe/1234567/isodate';
+    const url = 'http://localhost:8081/timestamp/'+autoUpdateTime;
+    console.log('timestamp value : '+autoUpdateTime);
+    http.open("GET", url);
+    http.send();
+    http.onreadystatechange = (e) => {
+        var response = http.responseText;
+        console.log(response);
+        var responseJSON = JSON.parse(response);
+        console.log('Data back frome Isodata server : '+responseJSON);
+        var isoDate = responseJSON.isoDate;
+        console.log('iso date: '+ isoDate);
+        document.getElementById('isodataHolderAuto').innerHTML= isoDate;
+       
+    }
+
+}
+
+function sendDataUtcServerAUpdateTime(autoUpdateTime){
+    var http = new XMLHttpRequest();
+    const url = 'http://localhost:8081/utcDate/'+autoUpdateTime;
+    console.log('timestamp value : '+autoUpdateTime);
+    http.open("GET", url);
+    http.send();
+    http.onreadystatechange = (e) => {
+        var response = http.responseText;
+        console.log(response);
+        var responseJSON = JSON.parse(response);
+        console.log('Data back frome Utcdata server : '+responseJSON);
+        var utcDate = responseJSON.date;
+        console.log('utc date: '+ utcDate);
+        document.getElementById('utcdataHolderAuto').innerHTML= utcDate;
+       
+    }
+
+}
